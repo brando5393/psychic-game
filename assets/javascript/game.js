098576b6ab4bdb7d9b57ad9ab611e151c9f7ff15
Brandon Williams <brando5393@gmail.com>
2019-12-14 09:42:27 -0600

    starting code in place

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+//Creates the array to store the letter selection of the computer at the beginning of each round
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+//Sores the computers choice from the array above
+var computerChoice =
